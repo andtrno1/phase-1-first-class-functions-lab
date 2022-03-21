77bda343dc40902a450e83759bf8c7a8a812c5c0
andtrno1 <andtrno1@gmail.ccom>
2022-03-20 22:09:11 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+// Code your solution in this file!{}
+ 
+const drivers =[];
+const returnFirstTwoDrivers = drivers => drivers.slice(0,2);
+
+const returnLastTwoDrivers = drivers => drivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers];
+
+const createFareMultiplier = function(fareMultiplier){
+    return function(a){
+        return fareMultiplier * a;
+
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const fareQuintupler = createFareMultiplier(4);
+
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers);
+}
